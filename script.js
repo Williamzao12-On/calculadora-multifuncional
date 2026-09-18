7485eded272bf8e1a631c14956ffa3fce4642ed5
william <willamzao12@gmail.com>
2026-09-18 10:46:32 -0300

    ci: adicionar automacao com GitHub Actions

--- script.js
+++ script.js
@@ -2,16 +2,44 @@ function somar() {
     let numero1 = Number(document.getElementById("numero1").value);
     let numero2 = Number(document.getElementById("numero2").value);
 
-    let resultado = numero1 + numero2;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 + numero2);
 }
+
 
 function subtrair() {
     let numero1 = Number(document.getElementById("numero1").value);
     let numero2 = Number(document.getElementById("numero2").value);
 
-    let resultado = numero1 - numero2;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 - numero2);
 }
+
+function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 * numero2);
+}
+
+
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+
+    if (numero2 === 0) {
+        document.getElementById("resultado").innerText =
+            "Resultado: Não é possível dividir por zero.";
+        return;
+    }
+
+
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 / numero2);
+}
+
